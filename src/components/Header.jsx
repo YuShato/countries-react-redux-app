d5227133a5e41bd5d52fdb263deacdcd273b00a9
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2022-02-24 10:33:33 +0300

    feat: add countries reducer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,6 @@ const ModeSwitcher = styled.div`
   color: var(--colors-text);
   font-size: var(--fs-sm);
   cursor: pointer;
-  // font-weight: var(--fw-bold);
   text-transform: capitalize;
 `
 
---
+++ src/store/countries/countries-reducer.js
@@ -0,0 +1,31 @@
+import { SET_COUNTRIES, SET_LOADING, SET_ERROR } from './countries-actions'
+const initialState = {
+  status: 'idle', //loading | received |  rejected
+  error: null,
+  list: []
+}
+
+export const counriesReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case SET_LOADING:
+      return {
+        ...state,
+        status: 'loading',
+        error: null
+      }
+    case SET_COUNTRIES:
+      return {
+        ...state,
+        status: 'received',
+        list: payload
+      }
+    case SET_ERROR:
+      return {
+        ...state,
+        status: 'rejected',
+        error: payload
+      }
+    default:
+      return state
+  }
+}
